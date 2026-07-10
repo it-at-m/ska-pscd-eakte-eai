8fd090763c6b1c88cdfd723fb608faeef3e408b2
florian.fischer <florian.fischer@muenchen.de>
2026-07-10 13:54:12 +0200

    reintroduced docs

--- eai/src/main/java/de/muenchen/oss/ska_pscd_eakte_eai/EAkteConnector.java
+++ eai/src/main/java/de/muenchen/oss/ska_pscd_eakte_eai/EAkteConnector.java
@@ -1,13 +1,12 @@
 package de.muenchen.oss.ska_pscd_eakte_eai;
 
+import de.muenchen.oss.ska_pscd_eakte_eai.data.PscdData;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EAkteConnector implements Processor {
-
-    private final Psc
 
     @Override
     public void process(Exchange exchange) throws Exception {
