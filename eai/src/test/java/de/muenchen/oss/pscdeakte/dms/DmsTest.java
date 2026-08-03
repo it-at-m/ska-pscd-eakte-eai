58aa512a3db3e95e03d0867be4294fe42551707c
florian.fischer <florian.fischer@muenchen.de>
2026-08-03 14:03:53 +0200

    wiremock
    DmsTest ok

--- eai/src/test/java/de/muenchen/oss/pscdeakte/dms/DmsTest.java
+++ eai/src/test/java/de/muenchen/oss/pscdeakte/dms/DmsTest.java
@@ -35,24 +35,27 @@ class DmsTest extends WiremockTest {
     @Test
     void createFileTest(){
         PscdData data = new PscdData();
-        data.setGpId("9000000003");
+        data.setGpId("9000000001");
         data.setName("Gebdat ohne Zeit");
         data.setVorname("Gebdat ohne Zeit");
         data.setGebDat("31.01.3210");
         data.setZentralaktkennung("9999");
         DmsObjektResponse response = dmsService.createFile(data, "COO.2150.8819.2.1120806");
         System.out.println(response.toString());
+        assertEquals("9512.999-9000000001-1", response.getObjname());
     }
 
     @Test
     void createProcedureBestandsakteTest(){
         DmsObjektResponse response = dmsService.createProcedureBestandsakte("COO.2150.8819.2.1120843");
         System.out.println(response.toString());
+        assertEquals("Bestandsakten (9512.999-1-0001)", response.getObjname());
     }
 
     @Test
     void createProcedureAVTest(){
         DmsObjektResponse response = dmsService.createProcedureAV("COO.2150.8819.2.1120843");
         System.out.println(response.toString());
+        assertEquals("AVs, Titel, Haftbefehle (9512.999-1-0002)", response.getObjname());
     }
 }
