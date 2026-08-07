1b345eed97450ac731a72521383962ffb3f62669
florian.fischer <florian.fischer@muenchen.de>
2026-08-07 14:46:57 +0200

    eaitest timeout

--- eai/src/test/java/de/muenchen/oss/pscdeakte/EaiTest.java
+++ eai/src/test/java/de/muenchen/oss/pscdeakte/EaiTest.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import de.muenchen.oss.pscdeakte.database.entity.PscdImport;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -21,18 +25,26 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles(TestConstants.SPRING_TEST_PROFILE)
 class EaiTest {
 
+    @Autowired
+    CamelContext ctx;
+
     @EndpointInject("mock:example")
     private MockEndpoint output;
 
     @Test
     void givenMessage_thenSendToMockShouldSucceed() throws InterruptedException {
+//        output.setAssertPeriod();
+//        output.setResultMinimumWaitTime(2000000);
+        long millis = TimeUnit.MINUTES.toMillis(30);
 
-        output.expectedMessageCount(1);
+        output.setResultWaitTime(millis);
+        ctx.getShutdownStrategy().setTimeout(millis);
+        output.expectedMessageCount(1272);
 
-        output.assertIsSatisfied();
+        output.assertIsSatisfied(millis);
 
         List<PscdImport> imports = output.getExchanges().getFirst().getMessage().getBody(List.class);
-        assertEquals(10, imports.size());
+        assertEquals(1272, imports.size());
 
     }
 
