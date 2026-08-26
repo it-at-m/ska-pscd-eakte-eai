7d85d9766c1d1286fadc1b428ae67fa014298a3e
florian.fischer <florian.fischer@muenchen.de>
2026-08-26 15:38:51 +0200

    flyway sql update

---
+++ eai/src/main/resources/db/migration/schema/V2__importErweitert_Log.sql
@@ -0,0 +1,33 @@
+CREATE SCHEMA IF NOT EXISTS pscdeakte;
+
+CREATE TABLE pscdeakte.pscd_import
+(
+
+    id                   SERIAL PRIMARY KEY,    -- INT PK
+    geschaeftspartner_id    VARCHAR(10),
+    name                    VARCHAR(50),
+    vorname                 VARCHAR(50),
+    geburtsdatum            VARCHAR(10),
+    zentralakt              VARCHAR(4),
+    betreffseinheit         VARCHAR(30),
+    akte                    VARCHAR(30),
+    bestandsakt             VARCHAR(30),
+    av                      VARCHAR(30),
+    status                  SMALLINT,
+    statustext              VARCHAR(50),
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at              TIMESTAMPTZ NOT NULL
+
+);
+
+CREATE TABLE pscdeakte.logs
+(
+
+    id                   SERIAL PRIMARY KEY,    -- INT PK
+    level                   VARCHAR(5),
+    message                 VARCHAR(50),
+    exception               VARCHAR(100),
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
+
