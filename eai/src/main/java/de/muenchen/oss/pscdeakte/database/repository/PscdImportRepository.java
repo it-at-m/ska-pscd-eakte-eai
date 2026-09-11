d4475d2e5edd4ed245f3c791d851e5476845a3a7
alexander.kerscher <alexander.kerscher@muenchen.de>
2026-09-11 14:29:12 +0200

    weitere Anmerkungen hinzugefuegt

--- eai/src/main/java/de/muenchen/oss/pscdeakte/database/repository/PscdImportRepository.java
+++ eai/src/main/java/de/muenchen/oss/pscdeakte/database/repository/PscdImportRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+// Review: Ggfs umstellen auf Jparepository anstelle von Crud
 public interface PscdImportRepository extends CrudRepository<PscdImport, Integer> {
 
     List<PscdImport> findByGeschaeftspartnerId(String geschaeftspartnerId);
--- eai/src/main/java/de/muenchen/oss/pscdeakte/dms/DmsService.java
+++ eai/src/main/java/de/muenchen/oss/pscdeakte/dms/DmsService.java
@@ -41,6 +41,12 @@ public class DmsService {
                 dmsProperties.getJobposition()).timeout(Duration.ofSeconds(30)).block();
     }
 
+    /**
+     *  Erzeugt in der Einzelakte eine Betreffseinheit
+     * @param laufendeNr laufende Nummer für das Geschaeftszeichen des uebergeordneten Aktenplaneintrags
+     * @param bereich Titel der anzulegenden Betreffseinheit
+     * @return Response der eAkte
+     */
     public DmsObjektResponse createSubjectAreaUnit(final int laufendeNr, final String bereich) {
         final CreateSubjectAreaUnitAnfrageDTO dto = new CreateSubjectAreaUnitAnfrageDTO();
         dto.setBasenr(dmsProperties.getAktenplannummer() + "." + laufendeNr);
@@ -50,22 +56,30 @@ public class DmsService {
                 dmsProperties.getJobposition()).timeout(Duration.ofSeconds(30)).block();
     }
 
+    /**
+     * Erzeugt in der Betreffseinheit eine Sachakte (Geschaeftspartner)
+     * @param data zu speichernde Daten der Sachakte
+     * @return Response der eAkte
+     */
     public DmsObjektResponse createFile(final PscdImport data) {
         final CreateFileDTO dto = new CreateFileDTO();
         //        TODO fallback falls data.getBetreffseinheit().isEmpty()?
         dto.shortname(data.getGeschaeftspartnerId()).filesubj(data.getZentralakt()).apentry(data.getBetreffseinheit()).definition(dmsProperties.getCooKmAkte());
         if (data.getVorname() != null && !data.getVorname().isEmpty()) {
             final UserFormsReferenz vornameReferenz = new UserFormsReferenz();
+            // Review: woher kommt der Name? Kann sich der ändern? ist egal
             vornameReferenz.lhMBAI151700Ufreference("BusinessDataGPFirstname").addLhMBAI151700UfvalueItem(data.getVorname());
             dto.addUserformsdataItem(vornameReferenz);
         }
         if (data.getName() != null && !data.getName().isEmpty()) {
             final UserFormsReferenz nameReferenz = new UserFormsReferenz();
+            // Review: woher kommt der Name? Kann sich der ändern?
             nameReferenz.lhMBAI151700Ufreference("BusinessDataGPSurname").addLhMBAI151700UfvalueItem(data.getName());
             dto.addUserformsdataItem(nameReferenz);
         }
         if (data.getGeburtsdatum() != null && !data.getGeburtsdatum().isEmpty()) {
             final UserFormsReferenz gebDatReferenz = new UserFormsReferenz();
+            // Review: woher kommt der Name? Kann sich der ändern?
             gebDatReferenz.lhMBAI151700Ufreference("BusinessDataGPBirthDate").addLhMBAI151700UfvalueItem(DateHelper.format(data.getGeburtsdatum()));
             dto.addUserformsdataItem(gebDatReferenz);
         }
@@ -73,17 +87,33 @@ public class DmsService {
                 .timeout(Duration.ofSeconds(30)).block();
     }
 
+    /**
+     * Bei der Erstellung einer Sachakte muss ein darin enthaltener Vorgang des Typs 'Bestandsakte' erzeugt werden.
+     * @param referrednumber uebergeordnete Sachakte
+     * @return Response der ProceduresApi
+     */
     public DmsObjektResponse createProcedureBestandsakte(final String referrednumber) {
-        final CreateProcedureDTO dto = new CreateProcedureDTO();
-        dto.shortname("Bestandsakten").accdef("Aktengebunden").referrednumber(referrednumber);
-        return proceduresApi
-                .createProcedure(dto, dmsProperties.getXAnwendung(), dmsProperties.getUserlogin(), dmsProperties.getJoboe(), dmsProperties.getJobposition())
-                .timeout(Duration.ofSeconds(30)).block();
+        return createProcedure(referrednumber, "Bestandsakten");
     }
 
+    /**
+     * Bei der Erstellung einer Sachakte muss ein darin enthaltener Vorgang des Typs 'AVs, Titel, Haftbefehle' erzeugt werden.
+     * @param referrednumber uebergeordnete Sachakte
+     * @return Response der ProceduresApi
+     */
     public DmsObjektResponse createProcedureAV(final String referrednumber) {
+        return createProcedure(referrednumber, "AVs, Titel, Haftbefehle");
+    }
+
+    /**
+     * Kapselt den Aufruf der ProceduresApi.
+     * @param referrednumber uebergeordnete Sachakte
+     * @param shortname Typ des anzulegenden Vorgangs
+     * @return Response der ProceduresApi
+     */
+    private DmsObjektResponse createProcedure(final String referrednumber, final String shortname) {
         final CreateProcedureDTO dto = new CreateProcedureDTO();
-        dto.shortname("AVs, Titel, Haftbefehle").accdef("Aktengebunden").referrednumber(referrednumber);
+        dto.shortname(shortname).accdef("Aktengebunden").referrednumber(referrednumber);
         return proceduresApi
                 .createProcedure(dto, dmsProperties.getXAnwendung(), dmsProperties.getUserlogin(), dmsProperties.getJoboe(), dmsProperties.getJobposition())
                 .timeout(Duration.ofSeconds(30)).block();
