a66f145280a853677752aff4b7e770d4bafd5cc8
florian.fischer <florian.fischer@muenchen.de>
2026-09-17 13:49:19 +0200

    ai vorschlag für die Annotation @LogExecutionTime

--- eai/src/main/java/de/muenchen/oss/pscdeakte/CsvToDb.java
+++ eai/src/main/java/de/muenchen/oss/pscdeakte/CsvToDb.java
@@ -1,25 +1,17 @@
 package de.muenchen.oss.pscdeakte;
 
-import de.muenchen.oss.pscdeakte.configuration.LogExecutionTime;
-import de.muenchen.oss.pscdeakte.database.DBLogger;
-import de.muenchen.oss.pscdeakte.database.DatensatzStatus;
-import de.muenchen.oss.pscdeakte.database.entity.PscdImport;
-import de.muenchen.oss.pscdeakte.database.repository.PscdImportRepository;
+
 import de.muenchen.oss.pscdeakte.s3.S3Properties;
 import de.muenchen.oss.refarch.integration.s3.application.port.out.S3OutPort;
 import de.muenchen.oss.refarch.integration.s3.domain.exception.S3Exception;
-import de.muenchen.oss.refarch.integration.s3.domain.model.FileReference;
 import de.muenchen.oss.refarch.integration.s3.domain.model.ListResult;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
+
 import org.springframework.stereotype.Component;
+import de.muenchen.oss.pscdeakte.service.CsvProcessingService;
 
 @Component
 @RequiredArgsConstructor
@@ -29,73 +21,15 @@ public class CsvToDb {
     @Getter
     private final S3OutPort s3;
     private final S3Properties props;
-    private final PscdImportRepository pir;
-    private final DBLogger logDb;
+    private final CsvProcessingService csvProcessingService;
 
-    enum HEADERS {
-        GP_ID,
-        NAME,
-        VORNAME,
-        GEB_DAT,
-        ZENTRALAKTKENNUNG
-    }
 
     public void processFiles() throws S3Exception {
         log.info("Processing CSV files");
         final ListResult list = s3.getFilesWithPrefix(props.getBucket(), props.getPrefix(), true);
         log.info("{} files found", list.files().size());
-        list.files().forEach(file -> saveFileToDb(file.path()));
+        list.files().forEach(file -> csvProcessingService.saveFileToDb(file.path()));
     }
 
-    @LogExecutionTime
-    private void saveFileToDb(final String filename) {
-        log.info("reading file {}", filename);
-        final CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
-                .setDelimiter(props.getDelimiter())
-                .setHeader(HEADERS.class)
-                .setSkipHeaderRecord(props.isSkipHeader())
-                .get();
-        final Iterable<CSVRecord> records;
-        try {
-            final FileReference fileReference = new FileReference(props.getBucket(), filename);
-            records = csvFormat.parse(new InputStreamReader(s3.getFileContent(fileReference), StandardCharsets.ISO_8859_1));
-            records.forEach(this::processCSVRecord);
-            final String movedFile = "." + filename;
-            s3.copyFile(fileReference, new FileReference(props.getBackupBucket(), movedFile));
-            s3.deleteFile(fileReference);
-            log.info("moved file {}/{} to {}/{}", props.getBucket(), filename, props.getBackupBucket(), movedFile);
-        } catch (IOException | S3Exception e) {
-            //            TODO Datenbankfehler abfangen
-            logDb.log("ERROR", "reading/moving file " + filename + " failed", e.getMessage());
-        }
-
-    }
-
-    @LogExecutionTime
-    private void processCSVRecord(final CSVRecord csvRecord) {
-        final PscdImport fromCsv = mapData(csvRecord);
-        final PscdImport fromDb = pir.findByGeschaeftspartnerId(fromCsv.getGeschaeftspartnerId());
-        if (fromDb == null) {
-            pir.save(fromCsv);
-            return;
-        }
-        final DuplicateOrUpdate dou = new DuplicateOrUpdate(fromDb, fromCsv);
-        if (dou.isUpdate()) {
-            pir.save(dou.createUpdatedPscdImport());
-        }
-    }
-
-    @LogExecutionTime
-    private PscdImport mapData(final CSVRecord csvRecord) {
-        log.debug("mapping GP {}", csvRecord.get(HEADERS.GP_ID));
-        final PscdImport data = new PscdImport();
-        data.setGeschaeftspartnerId(csvRecord.get(HEADERS.GP_ID));
-        data.setName(csvRecord.get(HEADERS.NAME));
-        data.setVorname(csvRecord.get(HEADERS.VORNAME));
-        data.setGeburtsdatum(csvRecord.get(HEADERS.GEB_DAT));
-        data.setZentralakt(csvRecord.get(HEADERS.ZENTRALAKTKENNUNG));
-        data.setStatus(DatensatzStatus.NEW);
-        return data;
-    }
 
 }
---
+++ eai/src/main/java/de/muenchen/oss/pscdeakte/service/CsvProcessingService.java
@@ -0,0 +1,91 @@
+package de.muenchen.oss.pscdeakte.service;
+
+import de.muenchen.oss.pscdeakte.configuration.LogExecutionTime;
+import de.muenchen.oss.pscdeakte.database.DBLogger;
+import de.muenchen.oss.pscdeakte.database.DatensatzStatus;
+import de.muenchen.oss.pscdeakte.database.entity.PscdImport;
+import de.muenchen.oss.pscdeakte.database.repository.PscdImportRepository;
+import de.muenchen.oss.pscdeakte.DuplicateOrUpdate;
+import de.muenchen.oss.pscdeakte.s3.S3Properties;
+import de.muenchen.oss.refarch.integration.s3.application.port.out.S3OutPort;
+import de.muenchen.oss.refarch.integration.s3.domain.exception.S3Exception;
+import de.muenchen.oss.refarch.integration.s3.domain.model.FileReference;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CsvProcessingService {
+
+    private final S3OutPort s3;
+    private final S3Properties props;
+    private final PscdImportRepository pir;
+    private final DBLogger logDb;
+
+    enum HEADERS {
+        GP_ID,
+        NAME,
+        VORNAME,
+        GEB_DAT,
+        ZENTRALAKTKENNUNG
+    }
+
+    @LogExecutionTime
+    public void saveFileToDb(final String filename) {
+        log.info("reading file {}", filename);
+        final CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
+                .setDelimiter(props.getDelimiter())
+                .setHeader(HEADERS.class)
+                .setSkipHeaderRecord(props.isSkipHeader())
+                .get();
+        try {
+            final FileReference fileReference = new FileReference(props.getBucket(), filename);
+            final Iterable<CSVRecord> records =
+                    csvFormat.parse(new InputStreamReader(s3.getFileContent(fileReference), StandardCharsets.ISO_8859_1));
+            for (final CSVRecord rec : records) {
+                processCSVRecord(rec);
+            }
+            final String movedFile = "." + filename;
+            s3.copyFile(fileReference, new FileReference(props.getBackupBucket(), movedFile));
+            s3.deleteFile(fileReference);
+            log.info("moved file {}/{} to {}/{}", props.getBucket(), filename, props.getBackupBucket(), movedFile);
+        } catch (IOException | S3Exception e) {
+            // TODO Datenbankfehler abfangen
+            logDb.log("ERROR", "reading/moving file " + filename + " failed", e.getMessage());
+        }
+    }
+
+    @LogExecutionTime
+    public void processCSVRecord(final CSVRecord csvRecord) {
+        final PscdImport fromCsv = mapData(csvRecord);
+        final PscdImport fromDb = pir.findByGeschaeftspartnerId(fromCsv.getGeschaeftspartnerId());
+        if (fromDb == null) {
+            pir.save(fromCsv);
+            return;
+        }
+        final DuplicateOrUpdate dou = new DuplicateOrUpdate(fromDb, fromCsv);
+        if (dou.isUpdate()) {
+            pir.save(dou.createUpdatedPscdImport());
+        }
+    }
+
+    @LogExecutionTime
+    public PscdImport mapData(final CSVRecord csvRecord) {
+        log.debug("mapping GP {}", csvRecord.get(HEADERS.GP_ID));
+        final PscdImport data = new PscdImport();
+        data.setGeschaeftspartnerId(csvRecord.get(HEADERS.GP_ID));
+        data.setName(csvRecord.get(HEADERS.NAME));
+        data.setVorname(csvRecord.get(HEADERS.VORNAME));
+        data.setGeburtsdatum(csvRecord.get(HEADERS.GEB_DAT));
+        data.setZentralakt(csvRecord.get(HEADERS.ZENTRALAKTKENNUNG));
+        data.setStatus(DatensatzStatus.NEW);
+        return data;
+    }
+}
