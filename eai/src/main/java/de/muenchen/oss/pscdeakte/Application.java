8a03ec6e3f55f763372fdd5cde2e4f5cb72ae0f6
alexander.kerscher <alexander.kerscher@muenchen.de>
2026-09-22 13:43:28 +0200

    update to scheduler

--- eai/src/main/java/de/muenchen/oss/pscdeakte/Application.java
+++ eai/src/main/java/de/muenchen/oss/pscdeakte/Application.java
@@ -1,34 +1,44 @@
 package de.muenchen.oss.pscdeakte;
 
+import de.muenchen.oss.pscdeakte.configuration.LogExecutionTime;
 import de.muenchen.oss.refarch.integration.s3.domain.exception.S3Exception;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @RequiredArgsConstructor
 @Slf4j
+@EnableScheduling
 @SuppressWarnings("PMD.UseUtilityClass")
 public class Application {
     private final CsvToDb csvToDb;
     private final DbToEakte dbToEakte;
 
-    public static void main(final String[] args) {
+    private static final AtomicBoolean stillRunning = new AtomicBoolean(false);
+
+    static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void csv2db2dms() throws S3Exception {
-        log.info("Loading CSV files");
-        this.csvToDb.processFiles();
-        log.info("Reading Database");
-        this.dbToEakte.start();
-        log.info("shutdown");
+    @Scheduled(cron = "${dms.cron}")
+    @LogExecutionTime
+    protected void scheduledTask() throws S3Exception {
+        if (!stillRunning.get()) {
+            stillRunning.set(true);
+            this.csvToDb.processFiles();
+            this.dbToEakte.start();
+            stillRunning.set(false);
+        } else {
+            log.info("Nothing to do, scheduled task still running");
+        }
     }
 
 }
