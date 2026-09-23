3a30d4612b2bd0ce2a656bb4dcb464d347d7506d
florian.fischer <florian.fischer@muenchen.de>
2026-09-23 14:10:03 +0200

    imports

--- eai/src/main/java/de/muenchen/oss/pscdeakte/Application.java
+++ eai/src/main/java/de/muenchen/oss/pscdeakte/Application.java
@@ -1,24 +1,28 @@
 package de.muenchen.oss.pscdeakte;
 
+import de.muenchen.oss.pscdeakte.configuration.LogExecutionTime;
 import de.muenchen.oss.refarch.integration.s3.domain.exception.S3Exception;
+import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @RequiredArgsConstructor
 @Slf4j
-@SuppressWarnings("PMD.UseUtilityClass")
+@EnableScheduling
 public class Application {
     private final CsvToDb csvToDb;
     private final DbToEakte dbToEakte;
 
-    public static void main(final String[] args) {
+    private static final AtomicBoolean STILL_RUNNING = new AtomicBoolean(false);
+
+    /* package */ static void main(final String... args) {
         SpringApplication.run(Application.class, args);
     }
 
@@ -28,7 +32,6 @@ public class Application {
         if (STILL_RUNNING.get()) {
             log.info("Nothing to do, scheduled task still running");
         } else {
-            log.info("starting");
             STILL_RUNNING.set(true);
             try {
                 this.csvToDb.processFiles();
