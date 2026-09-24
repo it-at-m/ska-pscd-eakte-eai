55d78bd35438fd42d4bf6a3cf1ef4df27c23a9c4
florian.fischer <florian.fischer@muenchen.de>
2026-09-24 12:01:05 +0200

    WiremockTest und abgeleitete Klassen auskommentiert

--- eai/src/test/java/de/muenchen/oss/pscdeakte/s3/S3IntegrationTest.java
+++ eai/src/test/java/de/muenchen/oss/pscdeakte/s3/S3IntegrationTest.java
@@ -1,71 +1,71 @@
-package de.muenchen.oss.pscdeakte.s3;
-
-import de.muenchen.oss.pscdeakte.CsvToDb;
-import de.muenchen.oss.pscdeakte.TestConstants;
-import de.muenchen.oss.pscdeakte.database.entity.PscdImport;
-import de.muenchen.oss.pscdeakte.database.repository.PscdImportRepository;
-import de.muenchen.oss.pscdeakte.dms.WiremockTest;
-import de.muenchen.oss.refarch.integration.s3.domain.model.FileReference;
-import java.io.File;
-
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles(TestConstants.SPRING_TEST_PROFILE)
-@Slf4j
-@Disabled
-class S3IntegrationTest extends WiremockTest {
-
-    private final S3Properties props;
-    private final CsvToDb csvToDb;
-    private final PscdImportRepository repo;
-
-    @Autowired
-    S3IntegrationTest(S3Properties props, CsvToDb csvToDb, PscdImportRepository repo) {
-        this.props = props;
-        this.csvToDb = csvToDb;
-        this.repo = repo;
-    }
-
-    @Test
-    void integratedTest() {
-        saveFile2DB("BP_Export_Test.csv");
-
-        final String gpId = "2000000000";
-        final PscdImport pi = repo.findByGeschaeftspartnerId(gpId);
-        Assertions.assertNotNull(pi);
-        Assertions.assertEquals(gpId, pi.getGeschaeftspartnerId());
-        Assertions.assertEquals("s3testname", pi.getName());
-        Assertions.assertEquals("s3testvorname", pi.getVorname());
-        Assertions.assertEquals("01.02.2012", pi.getGeburtsdatum());
-        Assertions.assertEquals("1234", pi.getZentralakt());
-
-        saveFile2DB("BP_Export_UpdateTest.csv");
-
-        final PscdImport updatePi = repo.findByGeschaeftspartnerId(gpId);
-        Assertions.assertNotNull(updatePi);
-        Assertions.assertEquals(gpId, updatePi.getGeschaeftspartnerId());
-        Assertions.assertEquals("s3testnameUpdate", updatePi.getName());
-        Assertions.assertEquals("s3testvornameUpdate", updatePi.getVorname());
-        Assertions.assertEquals("31.12.2012", updatePi.getGeburtsdatum());
-        Assertions.assertEquals("9876", updatePi.getZentralakt());
-
-        Assertions.assertDoesNotThrow(() -> repo.delete(updatePi));
-    }
-
-    private void saveFile2DB(final String filename) {
-        final FileReference origin = new FileReference(props.getBucket(), filename);
-        Assertions.assertDoesNotThrow(() -> csvToDb.getS3().saveFile(origin, new File("testdata/s3/" + filename)));
-        Assertions.assertDoesNotThrow(csvToDb::processFiles);
-        Assertions.assertDoesNotThrow(() -> Assertions.assertFalse(csvToDb.getS3().fileExists(origin)));
-        final FileReference backup = new FileReference(props.getBackupBucket(), "." + filename);
-        Assertions.assertDoesNotThrow(() -> Assertions.assertTrue(csvToDb.getS3().fileExists(backup)));
-        Assertions.assertDoesNotThrow(() -> csvToDb.getS3().deleteFile(backup));
-    }
-}
+//package de.muenchen.oss.pscdeakte.s3;
+//
+//import de.muenchen.oss.pscdeakte.CsvToDb;
+//import de.muenchen.oss.pscdeakte.TestConstants;
+//import de.muenchen.oss.pscdeakte.database.entity.PscdImport;
+//import de.muenchen.oss.pscdeakte.database.repository.PscdImportRepository;
+//import de.muenchen.oss.pscdeakte.dms.WiremockTest;
+//import de.muenchen.oss.refarch.integration.s3.domain.model.FileReference;
+//import java.io.File;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//@SpringBootTest
+//@ActiveProfiles(TestConstants.SPRING_TEST_PROFILE)
+//@Slf4j
+//@Disabled
+//class S3IntegrationTest extends WiremockTest {
+//
+//    private final S3Properties props;
+//    private final CsvToDb csvToDb;
+//    private final PscdImportRepository repo;
+//
+//    @Autowired
+//    S3IntegrationTest(S3Properties props, CsvToDb csvToDb, PscdImportRepository repo) {
+//        this.props = props;
+//        this.csvToDb = csvToDb;
+//        this.repo = repo;
+//    }
+//
+//    @Test
+//    void integratedTest() {
+//        saveFile2DB("BP_Export_Test.csv");
+//
+//        final String gpId = "2000000000";
+//        final PscdImport pi = repo.findByGeschaeftspartnerId(gpId);
+//        Assertions.assertNotNull(pi);
+//        Assertions.assertEquals(gpId, pi.getGeschaeftspartnerId());
+//        Assertions.assertEquals("s3testname", pi.getName());
+//        Assertions.assertEquals("s3testvorname", pi.getVorname());
+//        Assertions.assertEquals("01.02.2012", pi.getGeburtsdatum());
+//        Assertions.assertEquals("1234", pi.getZentralakt());
+//
+//        saveFile2DB("BP_Export_UpdateTest.csv");
+//
+//        final PscdImport updatePi = repo.findByGeschaeftspartnerId(gpId);
+//        Assertions.assertNotNull(updatePi);
+//        Assertions.assertEquals(gpId, updatePi.getGeschaeftspartnerId());
+//        Assertions.assertEquals("s3testnameUpdate", updatePi.getName());
+//        Assertions.assertEquals("s3testvornameUpdate", updatePi.getVorname());
+//        Assertions.assertEquals("31.12.2012", updatePi.getGeburtsdatum());
+//        Assertions.assertEquals("9876", updatePi.getZentralakt());
+//
+//        Assertions.assertDoesNotThrow(() -> repo.delete(updatePi));
+//    }
+//
+//    private void saveFile2DB(final String filename) {
+//        final FileReference origin = new FileReference(props.getBucket(), filename);
+//        Assertions.assertDoesNotThrow(() -> csvToDb.getS3().saveFile(origin, new File("testdata/s3/" + filename)));
+//        Assertions.assertDoesNotThrow(csvToDb::processFiles);
+//        Assertions.assertDoesNotThrow(() -> Assertions.assertFalse(csvToDb.getS3().fileExists(origin)));
+//        final FileReference backup = new FileReference(props.getBackupBucket(), "." + filename);
+//        Assertions.assertDoesNotThrow(() -> Assertions.assertTrue(csvToDb.getS3().fileExists(backup)));
+//        Assertions.assertDoesNotThrow(() -> csvToDb.getS3().deleteFile(backup));
+//    }
+//}
